2a613613928d417efbfdbbf8f5e07673a27778f6
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-24 19:58:53 -0500

    lists service and routes

--- Backend/routes/lists.js
+++ Backend/routes/lists.js
@@ -1,44 +1,106 @@
 var express = require('express');
 var router = express.Router();
-var Team = require('../models/team');
-var userService = require('../services/user.service');
+var List = require('../models/list');
+var listService = require('../services/listService');
 
 
-// add a list to a board
-router.post('/:boardId', function(req, res, next) {
 
-    Team.update({"boards._id": req.params.boardId},{$push: {"boards.$.lists": req.body}}
-    ,{new: true}, function (err, doc) {
-        if (err) res.status(500).send({error_code:1, msg:err});
-        else res.status(200).json({error_code:0})
-      });
+// get all lists
+router.get('/', function(req, res, next) {
+    listService.getLists()
+    .then(function (lists) {
+        if (lists) {
+            res.status(200).json(lists);
+        } else {
+            res.sendStatus(404);
+        }
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
 });
 
-// update a list
+
+// create a list and add it to a board
+router.post('/:id' /*board ID*/, function(req, res, next) {
+
+    delete req.body._id
+    var list = new Board(req.body)
+    console.log('boad : ' + list);
+    listService.createBoard(req.params.id , list)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
+
+});
+
+// update list name
 router.put('/:id' /*list ID*/, function(req, res, next) {
-    var team;
-    Team.find({'lists._id': req.params.id}, function (err, doc) {
-        if (err) res.status(500).send({error_code:1, msg:err});
-        else {
-                team= new Team(doc).boards
-            };
+    console.log(req.body);
+    listService.updateListName(req.params.id, req.body.name)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
+});
 
-    });       
+// add card to a list
+router.put('/cards/:id', function(req, res, next) {
+    console.log(req.body);
+    listService.addCard(req.params.id, req.body)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
+});
 
-    Team.boards.update({'lists._id': req.params.id},{'$set':{'lists.$.name':'hi'}}
-    ,{new: true}, function (err, doc) {
-        if (err) res.status(500).send({error_code:1, msg:err});
-        else res.status(200).json({error_code:0, _id: req.params.id})
-      });
+// update card name
+router.put('/cards/name/:id', function(req, res, next) {
+    console.log(req.body);
+    listService.updateListName(req.params.id, req.body.name)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
+});
+
+// remove card from a list
+router.delete('/cards/:id', function(req, res, next) {
+    listService.removeList(req.params.id)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
 });
 
 // remove a list from a board
 router.delete('/:id' /*list ID*/, function(req, res, next) {
 
-    Team.findOneAndUpdate({}, {$pull :{'lists':{'_id': req.params.id}}},  function (err) {
-        if (err) res.status(500).send({error_code:1, msg:err});
-        else res.status(200).json({error_code:0})
-      });
+    listService.deleteList(req.params.id)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+        res.status(400).send({error_code:1, msg:err});
+    });
 });
 
 module.exports = router;
